6e5bd8e0eb880935cd689d3e5da9e9f1dd7b336b
David Rock <davidrocha@outlook.com>
2021-02-28 14:49:22 -0300

    implement deposit

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -2,6 +2,7 @@ defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
   alias Rocketpay.User
+  alias Ecto.Repo
 
   action_fallback RocketpayWeb.FallbackController
 
@@ -11,5 +12,12 @@ defmodule RocketpayWeb.UsersController do
       |> put_status(:created)
       |> render("create.json", user: user)
     end
+  end
+
+  def all(conn) do
+    users = Repo.all(Rocketpay.User)
+    conn
+    |> put_status(:created)
+    |> render("all.json", users: users)
   end
 end
--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -10,7 +10,13 @@ defmodule RocketpayWeb.Router do
 
     get "/:filename", WelcomeController, :index
 
+    get "/users", UsersController, :all
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
+
+
   end
 
   # Enables LiveDashboard only for development
